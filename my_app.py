26b84c23e3b6e5fd31ea87938fc3e4b29b35ba6d
zaananasb <130475311+zaananasb@users.noreply.github.com>
2023-05-02 17:57:45 +0300

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -1,39 +1,76 @@
-from PyQt5.QtCore import Qt 
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # проверка типов вводимых значений
 from PyQt5.QtWidgets import (
-        QApplication, QWidget,
-        QHBoxLayout, QVBoxLayout,
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
 
 from instr import *
 from second_win import *
 
+
+     
 class MainWin(QWidget):
     def __init__(self):
-        super(),__init__()
-        self.initUI()
-        self.connects()
-        self.set_appear()
-        self.show()
-    def initUI(self):
-        self.btn_next = QPushButton(txt_next, self)
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = Qlabel(txt_instruction)
+        ''' окно, в котором располагается приветствие '''
+        super().__init__()
 
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        self.layout_line.addChildWidget(self.instruction, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
-        self.setLayout(self.layout_line)
+
+        #устанавливает, как будет выглядеть окно (надпись, размер, место)
+        self.set_appear()
+
+
+        # создаём и настраиваем графические элементы:
+        self.initUI()
+
+
+        #устанавливает связи между элементами
+        self.connects()
+
+
+        # старт:
+        self.show()
+
+
+    def initUI(self):
+        ''' создаёт графические элементы '''
+        self.btn_next = QPushButton(txt_next)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout)
+
+
+    
     def next_click(self):
         self.tw = TestWin()
         self.hide()
+
+
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
+
+
+   ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
-app = QApplication([])
-mw = MainWin()
-app.exet_()
+
+
+def main():
+   app = QApplication([])
+   mw = MainWin()
+   app.exec_()
+
+
+main()
+
+
